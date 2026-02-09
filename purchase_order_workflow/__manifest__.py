b7a3c1e04b9d58c92d94f72035de70749867dd73
GetApPRO <81932927+getappro@users.noreply.github.com>
2026-02-10 00:58:59 +0100

    Create manifest for Purchase Order Workflow module
    Initial manifest file for the Purchase Order Workflow module, including metadata such as name, version, category, and dependencies.

---
+++ purchase_order_workflow/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Purchase Order Workflow',
+    'version': '18.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Add Workflow to purchase order generate invoice .',
+    'description': """
+        Add Workflow to purchase order generate invoice .
+    """,
+    'author': 'GetapPRO',
+    'company': 'GetapPRO',
+    'maintainer': 'GetapPRO',
+    'website': 'https://www.getap.pro',
+    'depends': ['purchase.order'],
+    'data': [],
+    'demo': [],
+    'external_dependencies': {
+        'python': [],
+    },
+    #'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
